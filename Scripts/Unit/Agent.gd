b5046e5ffc39041dbe096ad333c51163bd02c7eb
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2026-07-05 01:07:25 -0500

    Agent and Clerk Unit creation
    Along with two buttons in main that don't do anything yet.

---
+++ Scripts/Unit/Agent.gd
@@ -0,0 +1,22 @@
+extends Unit
+class_name AgentUnit
+
+
+#@ Export Variables
+@export var max_health: int
+@export var current_health: int
+@export var max_mana: int
+@export var current_mana: int
+@export var max_sanity: int
+@export var current_sanity: int
+
+
+#@ Virtual Methods
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
