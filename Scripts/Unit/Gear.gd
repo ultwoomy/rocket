b7deeb9df5477146dabc413c63bcb66953f59960
ATengThao <58865845+LittleBeasty@users.noreply.github.com>
2026-07-06 21:07:29 -0500

    Level up function for Gear Resource

--- Scripts/Unit/Gear.gd
+++ Scripts/Unit/Gear.gd
@@ -14,3 +14,9 @@ enum GearType {
 @export var gear_type: GearType
 @export var unit_requirement: String  # ALERT: Type String for now since it isn't certain what the requirement is.
 @export var level: int = -1  # NOTE: Negative number means the gear does NOT level up.
+
+
+#@ Public Methods
+func level_up() -> void:
+	if level >= 0:
+		level += 1
